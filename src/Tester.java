e1e96af1fd84a01bfdcae040387ae80e66f012df
Chris-Haj <chrishaj1191@gmail.com>
2021-05-02 23:15:59 +0300

    1

---
+++ src/Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+    public static void main(String[] args) {
+        Integer[] a = {5, 7, 4, 8, 3, 6, 1};
+        BTree<Integer> tree = new BTree<>();
+        tree.add(7);
+        tree.add(4);
+        tree.add(9);
+        tree.toString();
+    }
+}
