cfc2ca01c9564eef67e70a042d4cc0ef8e1bd8bf
Chris-Haj <chrishaj1191@gmail.com>
2021-05-02 23:14:41 +0300

    1

--- src/BTree.java
+++ src/BTree.java
@@ -4,4 +4,20 @@ public class BTree <E>{
     public BTree() {
         root=null;
     }
+
+    public void add(E d){
+        if(root.getLeftSon().equals(null))
+            root.setLeftSon((Node<E>) d);
+        if(root.getRightSon().equals(null))
+            root.setRightSon((Node<E>)d);
+
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(root)+"\n"
+                +String.valueOf(root.getLeftSon())+"  "+String.valueOf(root.getRightSon());
+    }
 }
+
+
