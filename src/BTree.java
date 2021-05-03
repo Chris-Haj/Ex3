fab201f885abfff56aa3fc772dc0d3981b9388de
Chris-Haj <78118809+Chris-Haj@users.noreply.github.com>
2021-05-03 21:46:50 +0300

    Update BTree.java

--- src/BTree.java
+++ src/BTree.java
@@ -1,67 +1,67 @@
-public class BTree <E>{
+public class BTree <E> {
     private Node<E> root;
+    private int size = 0;
 
     public BTree() {
-        root=null;
+        this.root = null;
     }
 
-    public void add(E d){
-
-        if(root.getLeftSon()==(null)) {
+    public void add(E d) {
+        if (root == null) {
+            root = new Node<>(d);
+            size++;
+            return;
+        }
+        if (root.getLeftSon() == (null)) {
             root.setLeftSon(new Node<E>(d));
+            size++;
             return;
         }
-        if(root.getRightSon()==(null)) {
+        if (root.getRightSon() == (null)) {
             root.setRightSon(new Node<E>(d));
+            size++;
             return;
         }
 
-        if(Math.abs(recursiveHeight(root.getLeftSon()) - recursiveHeight(root.getRightSon()))<=1) {
+        if (Math.abs(recursiveHeight() - recursiveHeight()) <= 1) {
             root = root.getLeftSon();
             add(d);
-        }
-        else {
+        } else {
             root = root.getRightSon();
             add(d);
         }
 
     }
 
-    private int recursiveHeight(Node<E> node){
-        if(node.getLeftSon()==null)
+    private int recursiveHeight() {
+        if (root.getLeftSon() == null)
             return 0;
-        else{
-            return 1+recursiveHeight(node.getLeftSon());
+        else {
+            root = root.getLeftSon();
+            return 1 + recursiveHeight();
         }
     }
 
-    public String pre(){
-        String ordered="";
-        
+
+    public int height() {
+        return recursiveHeight();
     }
 
-    public int height(){
-        int height=0;
-        for(;root.getLeftSon()!=null;root= root.getLeftSon()){
-            height++;
-        }
-        return height;
-        }
-
-    public int size(){
+    public int size() {
         return checkSize(root);
     }
 
-    private int checkSize(Node<E> node){
-        if (node==null)
+    private int checkSize(Node<E> node) {
+        if (node == null)
             return 0;
-        else{
-            return 1+checkSize(node.getLeftSon())+checkSize(node.getRightSon());
+        else {
+            return 1 + checkSize(node.getLeftSon()) + checkSize(node.getRightSon());
         }
+
     }
 
-
-
 }
+
+
 
 
