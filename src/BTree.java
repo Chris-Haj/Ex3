b00dbb361475de39a252fee545b284a727685796
Chris-Haj <78118809+Chris-Haj@users.noreply.github.com>
2021-05-04 20:17:22 +0300

    Update BTree.java

--- src/BTree.java
+++ src/BTree.java
@@ -1,51 +1,84 @@
 public class BTree <E> {
     private Node<E> root;
-    private int size = 0;
+
 
     public BTree() {
         this.root = null;
+    }
+    public BTree(Node root) {
+        this.root=root;
+    }
+
+    public int getsum(){
+        return (int) root.getRightSon().getLeftSon().getData();
     }
 
     public void add(E d) {
         if (root == null) {
             root = new Node<>(d);
-            size++;
-            return;
-        }
-        if (root.getLeftSon() == (null)) {
-            root.setLeftSon(new Node<E>(d));
-            size++;
-            return;
-        }
-        if (root.getRightSon() == (null)) {
-            root.setRightSon(new Node<E>(d));
-            size++;
-            return;
-        }
 
-        if (Math.abs(recursiveHeight() - recursiveHeight()) <= 1) {
-            root = root.getLeftSon();
-            add(d);
-        } else {
-            root = root.getRightSon();
-            add(d);
+            return;
         }
+        else if (root.getLeftSon() == (null)) {
+            root.setLeftSon(new Node<E>(d));
+            return;
+        }
+        else if (root.getRightSon()  == (null)){
+            root.setRightSon(new Node<E>(d));
+            return;
+        }
+        addnode(root,d);
 
     }
 
-    private int recursiveHeight() {
-        if (root.getLeftSon() == null)
-            return 0;
-        else {
-            root = root.getLeftSon();
-            return 1 + recursiveHeight();
+    private void addnode(Node<E> node,E d) {
+        if(node.getLeftSon()==null){
+            node.setLeftSon(new Node<>(d));
+            return;
         }
+        else if(node.getRightSon()==null){
+            node.setRightSon(new Node<>(d));
+            return;
+        }
+
+        if(Math.abs(findHeight(node.getLeftSon())-findHeight(node.getRightSon())) <=1){
+        addnode(node.getLeftSon(), d);
+        }
+        else
+        addnode(node.getRightSon(),d);
+
+    }
+    
+
+
+
+    private void inOrder(Node<E> node) {
+        if (node == null) {
+            return;
+        }
+        else{
+        inOrder(node.getLeftSon());
+        System.out.print(node.getData()+"\t");
+        inOrder(node.getRightSon());
+        }
+    }
+    public void in() {
+        inOrder(root);
+    }
+
+    private int findHeight(Node node) {
+        if (node == null)
+            return 0;
+        else
+            return 1 + Math.max(findHeight(node.getLeftSon()), findHeight(node.getRightSon()));
+
     }
 
 
     public int height() {
-        return recursiveHeight();
+        return findHeight(root);
     }
+
 
     public int size() {
         return checkSize(root);
@@ -59,6 +92,8 @@ public class BTree <E> {
         }
 
     }
+
+
 
 }
 
